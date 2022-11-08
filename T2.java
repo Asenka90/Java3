f2833634c09a62a45e710eb11f1ab6a1e7d8adf6
Анастасия <89359100+Asenka90@users.noreply.github.com>
2022-11-08 14:50:48 +0400

    Add files via upload

---
+++ T2.java
@@ -0,0 +1,32 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+import java.util.ArrayList;
+import java.util.Random;
+
+public class T2 {
+    public static ArrayList<Integer> RemoveEven(ArrayList<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 0) {
+                nums.remove(i);
+                i--;
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.print("\033[H\033[2J");
+        Random rnd = new Random();
+        ArrayList<Integer> nums = new ArrayList<Integer>() {
+            {
+                for (int i = 0; i < 10; i++) {
+                    add(rnd.nextInt(-11, 11));
+                }
+            }
+        };
+        System.out.println(nums);
+        nums = RemoveEven(nums);
+        System.out.println(nums);
+
+    }
+}
